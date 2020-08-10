ac15f6cd5ffd1fafb1f3f373c47021383c54400c
umaepoch <umag@epochconsulting.in>
2020-08-10 14:38:46 +0530

    plastex commit

---
+++ plastex/api.py
@@ -0,0 +1,109 @@
+from __future__ import unicode_literals
+import frappe
+from frappe import _, throw, msgprint, utils
+from frappe.utils import cint, flt, cstr, comma_or, getdate, add_days, getdate, rounded, date_diff, money_in_words
+from frappe.model.mapper import get_mapped_doc
+from frappe.model.naming import make_autoname
+from erpnext.utilities.transaction_base import TransactionBase
+from erpnext.accounts.party import get_party_account_currency
+from frappe.desk.notifications import clear_doctype_notifications
+from datetime import datetime
+import sys
+import os
+import operator
+import frappe
+import json
+import time
+import math
+import base64
+import ast
+import schedule
+
+@frappe.whitelist()
+def get_email_list():
+	data = []
+	email_list = frappe.db.sql("""select email_id from `tabEmail Account` """,as_dict=1)
+	for em  in email_list:
+		data.append(em.email_id)
+	return data
+@frappe.whitelist()
+def get_print_format(doctype):
+	print_list = []
+	print_format = frappe.db.sql("""select name from `tabPrint Format` where doc_type = %s""",doctype,as_dict=1)
+	for pr in print_format:
+		print_list.append(pr.name)
+	return print_list
+@frappe.whitelist()
+def send_mail(values,docname):
+	from email.utils import formataddr
+	print_settings = frappe.get_doc("Print Settings", "Print Settings")
+	email_template = ""
+	content = ""
+	cc = ""
+	bcc = ""
+	values = json.loads(values)
+	if "email_template" in values:
+		email_template = values['email_template']
+	if 'content' in values:
+		content = values['content']
+	to = ""
+	recipients = ""
+	if 'recipients' in values:
+		to = values['recipients']
+		recipients = to.split(",")
+	
+	lang_1 = values['select_language']
+	lang_2 = values['select_languages']
+	cc_email = ""
+	if 'cc' in values:
+		cc = values['cc']
+		cc_email = cc.split(",")
+	bcc_email = ""
+	if 'bcc' in values:
+		bcc = values['bcc']
+		bcc_email = bcc.split(",")
+		
+	print_format1 = ""
+	if 'select_print_format' in values:
+		
+		print_format1 = values['select_print_format']
+	print_format2 = ""
+	if 'select_print_formats' in values:
+		print_format2 = values['select_print_formats']
+	purchase_order = 'Purchase Order'
+	sales_order = values['sales_order']
+	attachments = ""
+	if 'select_print_format' in values and 'select_print_formats' in values:
+		attachments = [{
+					"print_format_attachment": 1,
+					"doctype": purchase_order,
+					"name": docname,
+					"print_format": print_format1,
+					"print_letterhead": print_settings.with_letterhead,
+					"lang": lang_1
+				},
+				{
+					"print_format_attachment": 1,
+					"doctype": 'Sales Order',
+					"name": sales_order,
+					"print_format": print_format2,
+					"print_letterhead": print_settings.with_letterhead,
+					"lang": lang_2
+				}
+				]
+	else:
+		frappe.throw("Please select first print formats")
+	sender = frappe.get_list('Email Account', filters={"default_outgoing":1}, fields=['email_id'])
+	frappe.sendmail(recipients = recipients,
+			subject = values['subject'],
+			sender = sender[0]['email_id'],
+			cc = cc_email,
+			bcc = bcc_email,
+			#message = frappe.render_template(self.message, context),
+			message = content,
+			reference_doctype = "Purchase Order",
+			reference_name = docname,
+			attachments = attachments,
+			print_letterhead = ((attachments
+				and attachments[0].get('print_letterhead')) or False))
+	return True
---
+++ plastex/fixtures/custom_scripts/Purchase Order.js
@@ -0,0 +1,189 @@
+frappe.ui.form.on("Purchase Order", "refresh", function(frm, cdt, cdn) {
+    if (cur_frm.doc.docstatus == 1) {
+
+        cur_frm.add_custom_button(__('Send Mail'), function() {
+            if (cur_frm.doc.items[0].sales_order) {
+                var titles = "Purchase Order :" + cur_frm.doc.name
+                var email_list = get_email_list()
+                var print_list = get_print_format(cur_frm.doc.doctype)
+                var doctype = "Sales Order";
+                var print_lists = get_print_format(doctype)
+                var d = new frappe.ui.Dialog({
+                    title: titles,
+
+                    fields: [{
+                            label: 'TO',
+                            fieldname: 'recipients',
+                            fieldtype: 'MultiSelect',
+                            options: email_list
+                        },
+                        {
+                            label: 'CC, BCC & EMAIL TEMPLATE',
+                            fieldname: 'sec_1',
+                            fieldtype: 'Section Break',
+                            collapsible: 1
+                        },
+                        {
+                            label: 'CC',
+                            fieldname: 'cc',
+                            fieldtype: 'MultiSelect',
+                            options: email_list
+                        },
+                        {
+                            label: 'BCC',
+                            fieldname: 'bcc',
+                            fieldtype: 'MultiSelect',
+                            options: email_list
+                        },
+                        {
+                            label: 'Email Templat',
+                            fieldname: 'email_template',
+                            fieldtype: 'Link',
+                            options: "Email Template"
+                        },
+                        {
+                            label: '',
+                            fieldname: 'sec_2',
+                            fieldtype: 'Section Break',
+
+                        },
+                        {
+                            label: 'Subject',
+                            fieldname: 'subject',
+                            fieldtype: 'Data',
+                            default: titles
+                        }, {
+                            label: '',
+                            fieldname: 'sec_3',
+                            fieldtype: 'Section Break',
+
+                        },
+                        {
+                            label: 'Message',
+                            fieldname: 'content',
+                            fieldtype: 'Text Editor',
+
+                        },
+                        {
+                            label: '',
+                            fieldname: 'sec_3',
+                            fieldtype: 'Section Break',
+
+                        },
+                        {
+                            label: "Send me a copy",
+                            fieldname: 'send_me_a_copy',
+                            fieldtype: 'Check',
+                        },
+                        {
+                            label: "Send Read Receipt",
+                            fieldname: 'send_read_receipt',
+                            fieldtype: 'Check',
+                        },
+                        {
+                            label: "Attach Document Print",
+                            fieldname: 'attach_document_print',
+                            fieldtype: 'Check',
+                        },
+                        {
+                            label: "Select Print Format",
+                            fieldname: 'select_print_format',
+                            fieldtype: 'Select',
+                            options: print_list
+                        },
+                        {
+                            label: "Select Languages",
+                            fieldname: 'select_language',
+                            fieldtype: 'Link',
+                            options: "Language",
+                            default: "English"
+                        },
+                        {
+                            label: '',
+                            fieldname: 'col_break',
+                            fieldtype: 'Column Break',
+
+                        },
+                        {
+                            label: "Select Attachments",
+                            fieldname: "select_attachments",
+                            fieldtype: "Attach"
+                        },
+                        {
+                            label: "Sales Order",
+                            fieldname: "sales_order",
+                            fieldtype: "Link",
+                            options: "Sales Order",
+                            default: cur_frm.doc.items[0].sales_order,
+                            read_only: 1
+
+                        },
+                        {
+                            label: "Select Print Format",
+                            fieldname: 'select_print_formats',
+                            fieldtype: 'Select',
+                            options: print_lists
+                        },
+                        {
+                            label: "Select Languages",
+                            fieldname: 'select_languages',
+                            fieldtype: 'Link',
+                            options: "Language",
+                            default: "English"
+                        },
+                    ],
+                    primary_action_label: 'Send',
+                    primary_action(values) {
+                        d.hide();
+                        frappe.call({
+                            method: 'plastex.api.send_mail',
+                            args: {
+                                "values": d.get_values(),
+                                "docname": cur_frm.doc.name
+                            },
+                            async: false,
+                            callback: function(r) {
+                                email_list = r.message;
+
+                            }
+                        });
+                    }
+
+                })
+                d.show();
+            }
+        })
+    }
+})
+
+function get_email_list() {
+    var email_list = "";
+    frappe.call({
+        method: 'plastex.api.get_email_list',
+        args: {
+
+        },
+        async: false,
+        callback: function(r) {
+            email_list = r.message;
+
+        }
+    });
+    return email_list;
+}
+
+function get_print_format(doctype) {
+    var print_list = "";
+    frappe.call({
+        method: 'plastex.api.get_print_format',
+        args: {
+            "doctype": doctype
+        },
+        async: false,
+        callback: function(r) {
+            print_list = r.message;
+
+        }
+    });
+    return print_list
+}
